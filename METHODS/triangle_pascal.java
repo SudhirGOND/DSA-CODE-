44a186c24f7fe8774505020e7675692ee4afc96d
SUDHIR GOND <157738021+SudhirGOND@users.noreply.github.com>
2026-04-07 14:23:53 +0530

    Refactor triangle_pascal.java for Pascal's Triangle
    Updated the triangle_pascal.java file to improve formatting and structure for printing Pascal's Triangle.

--- METHODS/triangle_pascal.java
+++ METHODS/triangle_pascal.java
@@ -1,54 +1,43 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class Combinations {
-// public static void main(String[] args) {
-// Scanner sc = new Scanner(System.in);
-// System.out.print("Enter the N: ");
-// int N = sc.nextInt();
-// System.out.print("Enter the R: ");
-// int R = sc.nextInt();
-// System.out.printf("The NCR is %dC%d : %d", N, R, FACT(N) / (FACT(R) * FACT(N
-// - R)));
-// sc.close();
-// }
+// Program to print Pascal's Triangle using factorial method
+public class TrianglePascal {
 
-// import java.util.Scanner;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-// public class triangle_pascal {
+        // Input number of rows for Pascal's Triangle
+        System.out.print("Enter the number of Rows: ");
+        int I = sc.nextInt();
 
-//     public static void main(String[] args) {
+        // Outer loop for each row
+        for (int i = 0; i <= I; i++) {
 
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("Enter the Rows:I  ");
-//         int I = sc.nextInt();
-//         // System.out.print("Enter the Col:J ");
-//         // int J = sc.nextInt();
+            // Print spaces for proper alignment
+            for (int j = 0; j <= I - i; j++) {
+                System.out.print("  "); // Two spaces for formatting
+            }
 
-//         for (int i = 0; i <= I; i++) {
+            // Print the values in the row using combination formula
+            for (int j = 0; j <= i; j++) {
+                int ICJ = FACT(i) / (FACT(j) * FACT(i - j));
+                System.out.print(ICJ + "   "); // Extra spaces for spacing
+            }
 
-//             for (int j = 0; j <= I - i; j++) {///i is inceasing and the j is kaam ho rha hai!!
-//                 System.out.print("  ");
+            System.out.println(); // Move to next row
+        }
 
-//             }
+        sc.close();
+    }
 
-//             for (int j = 0; j <= i; j++) {
-//                 int ICJ = FACT(i) / (FACT(j) * FACT(i - j));
-//                 System.out.print(ICJ + "   ");
+    public static int FACT(int X) {
+        int XFACT = 1;
 
-//             }
+        // Multiply XFACT by each number from 2 to X
+        for (int i = 2; i <= X; i++) {
+            XFACT = XFACT * i;
+        }
 
-//             System.out.println();
-
-//         }
-
-//         sc.close();
-//     }
-
-//     public static int FACT(int X) {
-//         int XFACT = 1;
-//         for (int i = 2; i <= X; i++) {
-//             XFACT = XFACT * i;
-//         }
-//         return XFACT;
-//     }
-// }
+        return XFACT;
+    }
+}
